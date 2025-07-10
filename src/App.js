ad845959c9f152b3e10579c7d7da553926deb034
Aminjon <kuchimov.amin@mail.ru>
2025-07-10 17:52:11 +0300

    Решил проблему, добавил корзину и сделал Link

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
 import React from "react";
+
+import { Routes, Route } from "react-router-dom";
 import "./scss/app.scss";
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  const [items, setItems] = React.useState([]);
-
-  React.useEffect(() => {
-    fetch("https://686ba7e1e559eba908737d45.mockapi.io/items")
-      .then((res) => {
-        return res.json();
-      })
-      .then((arr) => {
-        setItems(arr);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock
-                key={obj.id}
-                title={obj.title}
-                price={obj.price}
-                imageUrl={obj.imageUrl}
-                sizes={obj.sizes}
-                types={obj.types}
-              />
-            ))}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const Skeleton = (props) => (
+  <ContentLoader
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={466}
+    viewBox="0 0 280 466"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <circle cx="134" cy="110" r="110" />
+    <rect x="6" y="237" rx="9" ry="9" width="275" height="22" />
+    <rect x="3" y="271" rx="10" ry="10" width="280" height="88" />
+    <rect x="6" y="371" rx="10" ry="10" width="95" height="27" />
+    <rect x="126" y="366" rx="25" ry="25" width="152" height="45" />
+  </ContentLoader>
+);
+
+export default Skeleton;
